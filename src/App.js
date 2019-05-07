00a7b373a01d88d8475e7b1caa00789d222d5128
lakhansamani <lakhan.m.samani@gmail.com>
2019-05-07 06:54:36 +0530

    chore: add webpack + react + babel config

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Welcome to react app</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WriteWebPackPlugin = require('write-file-webpack-plugin');
+
+const { NODE_ENV } = process.env;
+
+const plugins = [
+	// Ignore all locale files of moment.js
+	new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+	new CleanWebpackPlugin({
+		cleanOnceBeforeBuildPatterns: ['dist/app'],
+	}),
+	new HtmlWebpackPlugin({
+		template: './public/index.html',
+		alwaysWriteToDisk: true,
+	}),
+	new HtmlWebpackHarddiskPlugin(),
+	new CopyWebpackPlugin(['./public/favicon.ico', './public/manifest.json']),
+	new WriteWebPackPlugin(),
+];
+
+const isDevelopment = NODE_ENV === 'development';
+
+module.exports = {
+	mode: NODE_ENV || isDevelopment,
+	entry: ['./src/index.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		filename: isDevelopment ? '[name].js' : '[name].[chunkhash:8].js',
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'initial',
+		},
+	},
+	plugins,
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							insertAt: 'top',
+						},
+					},
+					'css-loader',
+				],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: ['file-loader'],
+			},
+		],
+	},
+};
