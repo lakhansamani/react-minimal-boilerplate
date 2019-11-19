c9b65fc75982f19a5b51b9c1a607bd092830038f
Michał Burdzy <michal.burdzy@netguru.com>
2019-11-19 12:57:03 +0100

    chore: updated dependencies. Works with npm and yarn

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('renders app', async () => {
