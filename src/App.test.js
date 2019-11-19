d968a68573abffc82f9b98e9565840c2236e4faa
Michał Burdzy <michal.burdzy@netguru.com>
2019-11-19 13:05:14 +0100

    fix: replaced "react-testing-library" with @testing-library/react

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
